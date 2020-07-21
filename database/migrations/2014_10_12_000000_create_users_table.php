31d7ab989d0ca8f7cb91789ed2f4be92b87ddf2e
Ram Krishnan <ramkrishnan@live.com>
2020-07-21 18:53:29 +0530

    Add API Token for user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ class CreateUsersTable extends Migration
             $table->enum('role', ['admin', 'customer', 'seller'])->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('api_token')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
