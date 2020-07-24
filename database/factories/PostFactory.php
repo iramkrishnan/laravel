d62ee24adee0bd257dc7049ff4a1757f724ea8c2
Ram Krishnan <ramkrishnan@live.com>
2020-07-24 11:44:54 +0530

    Add Post Factory and seeder

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -3,6 +3,7 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\Post;
+use App\User;
 use Faker\Generator as Faker;
 
 $factory->define(Post::class, function (Faker $faker) {
@@ -11,3 +12,11 @@ $factory->define(Post::class, function (Faker $faker) {
         'body' => $faker->paragraph(3),
     ];
 });
+
+//$factory->define(Post::class, function ($faker) {
+//    return [
+//        'title' => $faker->sentence,
+//        'body' => $faker->paragraph,
+//        'user_id' => factory(User::class),
+//    ];
+//});
