90b8b9f6b862ae3a92449fd7ea7b148595e1e984
Ram Krishnan <ramkrishnan@live.com>
2020-07-24 14:20:57 +0530

    Use Manual Validator in Post Controller

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StorePost;
 use App\Http\Resources\PostResource;
 use App\Post;
+use App\Rules\NoSpecialChars;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class PostController extends Controller
 {
@@ -17,12 +20,43 @@ class PostController extends Controller
         return new PostResource($posts);
     }
 
-    public function store(StorePost $request)
+    public function store(Request $request) // Can use StorePost
     {
+//        $request->validate([
+//            'title' => 'bail|required|unique:posts|max:255',
+//            'body.body' => ['required'],
+//        ]);
+
+        $rules = [
+            'title' => ['bail', 'required', 'unique:posts', 'max:255', new NoSpecialChars],
+            'body' => 'required',
+            'sub_title' => 'exclude_unless:title,|required',
+        ];
+
+        $messages = [
+            'title.required' => 'A :attribute is required',
+            'body.required' => 'A :attribute is required',
+            'sub_title.required' => 'A :attribute is required',
+        ];
+
+        $attributes = [
+            'title' => 'Title',
+            'body' => 'Body',
+            'sub_title' => 'Sub Title'
+        ];
+
+        $validated = Validator::make($request->all(), $rules, $messages, $attributes);
+
+        $validated->after(function ($validated) use ($request) {
+            if (is_numeric($request['title'])) {
+                $validated->errors()->add('title', 'Title cannot be an integer');
+            }
+        })->validate();
+
         $post = new Post();
         $post->user_id = request()->user()->id;
-        $post->title = $request->validated()['title'];
-        $post->body = $request->validated()['body'];
+        $post->title = $validated->validated()['title'];
+        $post->body = $validated->validated()['body'];
         $post->save();
 
         return $post;
